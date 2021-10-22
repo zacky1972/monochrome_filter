576691b3b297c6fd834e2964d960a2a1e9c45b1d
Susumu Yamazaki <zacky1972@gmail.com>
2021-10-23 05:23:08 +0900

    remove hello

--- lib/monochrome_filter.ex
+++ lib/monochrome_filter.ex
@@ -2,17 +2,4 @@ defmodule MonochromeFilter do
   @moduledoc """
   Documentation for `MonochromeFilter`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MonochromeFilter.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/monochrome_filter_test.exs
+++ test/monochrome_filter_test.exs
@@ -1,8 +1,4 @@
 defmodule MonochromeFilterTest do
   use ExUnit.Case
   doctest MonochromeFilter
-
-  test "greets the world" do
-    assert MonochromeFilter.hello() == :world
-  end
 end
