c4beafbb3ea96c8a94390b644e71e80e684694e9
Susumu Yamazaki <zacky1972@gmail.com>
2021-10-23 08:11:10 +0900

    revise monochrome32

--- c_src/monochrome32.c
+++ c_src/monochrome32.c
@@ -5,7 +5,7 @@ void monochrome32(uint64_t size, uint8_t *in, uint8_t *out)
 {
     uint8_t *pin = in;
     uint8_t *pout = out;
-    for(u_int64_t i = 0; i < size; i++) {
+    for(uint64_t i = 0; i < size; i++) {
         uint8_t r = *pin++;
         uint8_t g = *pin++;
         uint8_t b = *pin++;
