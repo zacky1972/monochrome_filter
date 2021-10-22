64d40cf538596c59df008dda795706e4ce4f82ef
Susumu Yamazaki <zacky1972@gmail.com>
2021-10-23 05:39:12 +0900

    create MonochromeFilter

--- lib/monochrome_filter.ex
+++ lib/monochrome_filter.ex
@@ -1,5 +1,45 @@
 defmodule MonochromeFilter do
+  import Nx.Defn
+
   @moduledoc """
   Documentation for `MonochromeFilter`.
   """
+
+  defn transpose_vector(vector) do
+    Nx.reshape(vector, {Nx.size(vector), 1})
+  end
+
+  defn broadcast_vector(vector, shape_tensor) do
+    vector
+    |> transpose_vector()
+    |> Nx.broadcast(shape_tensor)
+  end
+
+  defn monochrome_filter_32(pixel) do
+    assert_shape_pattern pixel, {_, 3}
+
+    mono = Nx.tensor([0.399, 0.587, 0.114], type: {:f, 32})
+    pixel_m = Nx.dot(pixel, mono)
+
+    broadcast_vector(pixel_m, pixel)
+    |> Nx.round()
+    |> Nx.as_type({:u, 8})
+  end
+
+  defn monochrome_filter_16(pixel) do
+    assert_shape_pattern pixel, {_, 3}
+
+    mono = Nx.tensor([0.399, 0.587, 0.114], type: {:f, 16})
+    pixel_m = Nx.dot(pixel, mono)
+
+    broadcast_vector(pixel_m, pixel)
+    |> Nx.round()
+    |> Nx.as_type({:u, 8})
+  end
+
+  defn init_pixel() do
+    Nx.tensor([0x9f, 0x5a, 0xae], type: {:u, 8})
+    |> broadcast_vector(Nx.iota({3, 256}))
+    |> Nx.transpose()
+  end
 end
