4c1b49e826ccf5c1c4c5b27db92d13eb7e7e6d11
Susumu Yamazaki <zacky1972@gmail.com>
2021-10-23 20:17:21 +0900

    revise in case of aarch64

--- c_src/monochrome16i.c
+++ c_src/monochrome16i.c
@@ -1,7 +1,7 @@
 #include "monochrome16i.h"
 #include <stdint.h>
 
-#if ! defined(__arm64__)
+#if ! defined(__arm64__) && ! defined(__aarch64__)
 void monochrome16i(uint64_t size, uint8_t *in, uint8_t *out)
 {
     uint8_t *pin = in;
@@ -11,6 +11,25 @@ void monochrome16i(uint64_t size, uint8_t *in, uint8_t *out)
         uint8_t g = *pin++;
         uint8_t b = *pin++;
         uint8_t m = r * 0.299f + g * 0.587f + b * 0.114f;
+        *pout++ = m;
+        *pout++ = m;
+        *pout++ = m;
+    }
+}
+#elif defined(__aarch64__)
+static const __fp16 mono_r = 0.299;
+static const __fp16 mono_g = 0.587;
+static const __fp16 mono_b = 0.114;
+
+void monochrome16(uint64_t size, uint8_t *in, uint8_t *out)
+{
+    uint8_t *pin = in;
+    uint8_t *pout = out;
+    for(uint64_t i = 0; i < size; i++) {
+        uint8_t r = *pin++;
+        uint8_t g = *pin++;
+        uint8_t b = *pin++;
+        uint8_t m = (__fp16)r * mono_r + (__fp16)g * mono_g + (__fp16)b * mono_b;
         *pout++ = m;
         *pout++ = m;
         *pout++ = m;
