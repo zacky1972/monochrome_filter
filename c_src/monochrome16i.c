a763cc582265e7e8aed265ba7becf2af9f998f58
Susumu Yamazaki <zacky1972@gmail.com>
2021-10-24 12:18:36 +0900

    remove for clang 13

--- c_src/monochrome16i.c
+++ c_src/monochrome16i.c
@@ -16,7 +16,7 @@ void monochrome16i(uint64_t size, uint8_t *in, uint8_t *out)
         *pout++ = m;
     }
 }
-#elif ! defined(__arm64__) && ! ( defined(__clang__) && __clang_major__ >= 13)
+#elif ! defined(__arm64__)
 static const __fp16 mono_r = 0.299;
 static const __fp16 mono_g = 0.587;
 static const __fp16 mono_b = 0.114;
@@ -35,7 +35,7 @@ void monochrome16i(uint64_t size, uint8_t *in, uint8_t *out)
         *pout++ = m;
     }
 }
-#else // defined(__arm64__) || ( defined(__clang__) && __clang_major__ >= 13)
+#else // defined(__arm64__)
 
 #include <arm_neon.h>
 
