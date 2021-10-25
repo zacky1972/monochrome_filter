bced9322a98f30988b206163fd4adf28c36bae51
Susumu Yamazaki <zacky1972@gmail.com>
2021-10-26 05:30:45 +0900

    for v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MonochromeFilter.MixProject do
   def project do
     [
       app: :monochrome_filter,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
